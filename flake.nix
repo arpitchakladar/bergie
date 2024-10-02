47acaa71caa341610c315095ace28e7b33d28a19
Arpit Chakladar <54011232+arpitchakladar@users.noreply.github.com>
2024-10-02 18:52:12 +0530

    Made few minor changes to the configuration.

--- flake.nix
+++ flake.nix
@@ -1,17 +1,36 @@
 {
-	description = "A very basic flake";
+	description = "Flake for development environment for bergie.";
 
 	inputs = {
 		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 	};
 
-	outputs = { self, nixpkgs }: {
+	outputs = { self, nixpkgs }:
+	let
+		pkgs = nixpkgs.legacyPackages."x86_64-linux";
+		buildScript = pkgs.writeShellScriptBin "run" ''
+#!/bin/sh
+cmake -B build
+make -C build && ./build/bergie
+		'';
+	in
+	{
 		devShells."x86_64-linux".default = pkgs.mkShell {
 			packages = with pkgs; [
+				(clang-tools.override {
+					enableLibcxx = false;
+				})
 				clang
 				cmake
 				glfw
+				glew
+				buildScript
 			];
+
+			shellHook = ''
+export CC="${pkgs.clang}/bin/clang"
+export CXX="${pkgs.clang}/bin/clang++"
+			'';
 		};
 	};
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-int main(void)
-{
+#include <iostream>
+
+int main(void) {
 	GLFWwindow* window;
 
 	/* Initialize the library */
@@ -10,8 +12,7 @@ int main(void)
 
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-	if (!window)
-	{
+	if (!window) {
 		glfwTerminate();
 		return -1;
 	}
@@ -19,11 +20,21 @@ int main(void)
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
+	if (glewInit() != GLEW_OK)
+		return -1;
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
 	/* Loop until the user closes the window */
-	while (!glfwWindowShouldClose(window))
-	{
+	while (!glfwWindowShouldClose(window)) {
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBegin(GL_TRIANGLES);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f(0.0f, 0.5f);
+		glVertex2f(0.5f, -0.5f);
+		glEnd();
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
